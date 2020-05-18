63c029908173f54b5ae38641ae4a910ba6fe28a5
Saurabh <singh97saurabh19@gmail.com>
2020-05-18 17:20:12 +0530

    second

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
 from .models import UserData, Activity
 
-
+# converting data of Activity model to json
 class ActivityPeriodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Activity
         fields = ["start_time", "end_time"]
 
-
+# converting data of user data to json 
 class UserSerializer(serializers.ModelSerializer):
     activity_periods = ActivityPeriodSerializer(many=True, read_only=True)
 
--- myapp/views.py
+++ myapp/views.py
@@ -5,12 +5,12 @@ from rest_framework.response import Response
 from .serializers import UserSerializer
 from django.views.generic import CreateView
 
-# Create your views here.
+# Homepage
 def home(request):
 
     return render(request, "myapp/base.html", {})
 
-
+# Api Call for displaying data
 class ArticalAPIView(APIView):
     def get(self, request):
         articles = models.UserData.objects.all()
@@ -18,13 +18,13 @@ class ArticalAPIView(APIView):
         data_member = {"ok": "true", "members": serializer.data}
         return Response(data_member)
 
-
+# For Adding data to user model
 class UserCreateView(CreateView):
 
     form_class = forms.UserForm
     model = models.UserData
 
-
+# for adding data to user model
 class ActivityCreateView(CreateView):
 
     form_class = forms.ActivityForm
